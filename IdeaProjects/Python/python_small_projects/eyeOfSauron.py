a65883b89cfba4eb8a53a00c1714576a01ac950a
Artur Barcij <arturbarcij1@gmail.com>
2023-12-20 17:39:25 +0100

    Add files via upload

---
+++ IdeaProjects/Python/python_small_projects/eyeOfSauron.py
@@ -0,0 +1,15 @@
+s = input()
+# Split the string based on "()"
+parts = s.split("()")
+
+# Extract the prefix and suffix
+prefix = parts[0]
+suffix = parts[1] if len(parts) > 1 else ""
+
+if (s.startswith('|') and s.endswith('|')):
+    if prefix == suffix:
+        print("correct")
+    else:
+        print("fix")
+else:
+    print("fix")
