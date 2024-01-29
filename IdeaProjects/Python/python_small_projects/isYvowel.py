e4ef0a85ccb7e0faa278ce72db6f34f351491995
Artur Barcij <arturbarcij1@gmail.com>
2024-01-29 17:05:29 +0100

    Add files via upload

---
+++ IdeaProjects/Python/python_small_projects/isYvowel.py
@@ -0,0 +1,4 @@
+l = input()
+
+print(l.count('a') + l.count('e') + l.count('i') + l.count('o') + l.count('u'), end = " ")
+print(l.count('a') + l.count('e') + l.count('i') + l.count('o') + l.count('u') + l.count('y'))
